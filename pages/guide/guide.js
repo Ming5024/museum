32d1ee5f07872946e81dc39f6a23a2d9fd8114c2
huanghaodong1997 <517998494@qq.com>
2018-08-16 21:32:57 +0800

    guide

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -10,13 +10,59 @@ Page({
     // tab切换  
     currentTab: 0,
 	
-    pics:[
-      '/images/1F.jpg',
-      '/images/2F.jpg',
-      '/images/3F.jpg'
-    ]
+     pics:{},
+   
+    touch: {
+      distance: 0,
+      scale: 1,
+      baseWidth: null,
+      baseHeight: null,
+      scaleWidth: null,
+      scaleHeight: null
+    }
+    
 
   },
+  touchstartCallback: function (e) {
+    // 单手指缩放开始，也不做任何处理 if(e.touches.length == 1) return console.log('双手指触发开始')
+    // 注意touchstartCallback 真正代码的开始 // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug // 当两根手指放上去的时候，就将distance 初始化。 let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    this.setData({
+      'touch.distance': distance,
+    })
+  },
+  touchmoveCallback: function (e) {
+    let touch = this.data.touch
+    // 单手指缩放我们不做任何操作 if(e.touches.length == 1) return console.log('双手指运动')
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    // 新的 ditance let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    let distanceDiff = distance - touch.distance;
+    let newScale = touch.scale + 0.005 * distanceDiff
+    {
+    newScale = 2
+  }
+        if(newScale <= 0.6) {
+  newScale = 0.6
+}
+let scaleWidth = newScale * touch.baseWidth
+let scaleHeight = newScale * touch.baseHeight
+({
+'touch.distance': distance,
+  'touch.scale': newScale,
+    'touch.scaleWidth': scaleWidth,
+      'touch.scaleHeight': scaleHeight,
+        'touch.diff': distanceDiff
+        })
+  }, bindload: function (e) {
+   ({
+    'touch.baseWidth': e.detail.width,
+      'touch.baseHeight': e.detail.height,
+        'touch.scaleWidth': e.detail.width,
+          'touch.scaleHeight': e.detail.height
+  })
+    },
   onLoad: function () {
     var that = this;
 
@@ -33,6 +79,20 @@ Page({
       }
 
     });
+
+    wx.request({
+      url: 'http://172.18.233.8:52080/search/mainintro',
+      method: 'GET',
+      success: res => {
+        for (var i = 0; i < res.data.guide_pic.length; i++) {
+          res.data.guide_pic[i] = "http://172.18.233.8:52080/pic/exhibitionpic/" + res.data.guide_pic[i];
+        }
+        this.setData({
+          intro: "&nbsp;&nbsp;" + res.data.simple_intro.replace("\r\n", "\r\n&nbsp;&nbsp;"),
+          pics: res.data.guide_pic
+        })
+      }
+    })
   },
  
   /** 
